ad0c27dbd825629c68e68c3a4a5bf6f98c6de0c6
jonathancone <jonathan.t.cone@gmail.com>
2015-09-23 20:15:31 -0500

    Updating copyright and JavaDoc.

--- src/main/java/indo/sql/RowProcessor.java
+++ src/main/java/indo/sql/RowProcessor.java
@@ -1,11 +1,38 @@
+/*
+ * Copyright 2015 Indo Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package indo.sql;
 
 import java.sql.ResultSet;
 
 /**
- * Created by JCone on 9/17/2015.
+ * This functional interface allows for custom row processing for SQL result
+ * sets.
+ *
+ * @author Jonathan Cone
  */
 @FunctionalInterface
 public interface RowProcessor<T> {
+    /**
+     * Maps a {@link ResultSet} to an object with the assumption that the
+     * ResultSet cursor is correctly pointing to the row that should be
+     * processed.
+     *
+     * @param rs The result set to transform.
+     * @return an object specified by the parameterized type.
+     */
     T map(ResultSet rs);
 }
